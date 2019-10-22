905648c37956e5d0d56fb576e3db924b1f090a55
Quan Nguyen <huu2uan@gmail.com>
2019-10-22 16:12:33 +0700

    Add proto

--- 3.grpc-app/project.clj
+++ 3.grpc-app/project.clj
@@ -8,5 +8,14 @@
                  [http-kit "2.3.0"]
                  [compojure "1.6.1"]
                  [metosin/muuntaja "0.6.5"]]
+  
   :main ^:skip-aot hello-world.core
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  
+  :plugins [[lein-protoc "0.5.0"]]
+  
+  :protoc-version "3.10.0"
+  :protoc-grpc {:version "1.24.0"}
+                      
+  :source-paths ["src/clj"]
+  :java-source-paths ["target/generated-sources/protobuf"])
